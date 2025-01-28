04fc62cd567932df6b9ea8d99cb8a65d86bbb4d4
Julio Fuertes <juliofuertestarin@gmail.com>
2025-01-28 10:17:18 +0100

    getting some form action in this biatch

--- src/app/admin/[collection]/[id]/_components/CollectionItemFormModule.tsx
+++ src/app/admin/[collection]/[id]/_components/CollectionItemFormModule.tsx
@@ -1,19 +1,24 @@
 'use client'
 
+import { formSchemaActionIsUpdateAction, getFormSchema } from "@/lib/forms"
 import Form from "@/lib/forms/Form"
-import { getFormByCollectionName } from "@/lib/forms/collection"
 import { ICollectionNames, IAllCollections } from "@/types"
 import { IActionTarget } from "@/types/forms"
 
 export default function CollectionItemFormModule({ collection, collectionItem, target } : { collection: ICollectionNames, collectionItem: IAllCollections, target: IActionTarget }) {
 
-    const { action, fields } = getFormByCollectionName({ collection, actionTarget: 'update' })
+    const { getBoundAction, fields } = getFormSchema({ collection, actionTarget: 'update' })
+
+    if (!formSchemaActionIsUpdateAction(getBoundAction, 'update'))
+        throw new Error('Error generating bound action in form!')
+
+    const boundAction = getBoundAction({ id: collectionItem.id })
 
     return (
         <div className="w-full max-w-xl">
             <Form 
                 target={ target }
-                action={ action }
+                boundAction={ boundAction }
                 fields={ fields }
                 collection={ collection }
                 collectionItem={ collectionItem }
